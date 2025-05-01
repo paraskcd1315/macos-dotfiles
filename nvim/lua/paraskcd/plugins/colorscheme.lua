92c450506168df3345f61a3986ca329b7043e251
Paras Khanchandani <dev@paraskcd.com>
2025-05-01 22:45:02 +0200

    Add btop and ghostty settings, change nvim transparent

--- nvim/lua/paraskcd/plugins/colorscheme.lua
+++ nvim/lua/paraskcd/plugins/colorscheme.lua
@@ -1,10 +1,27 @@
 return {
-    "folke/tokyonight.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-        require("tokyonight").setup({
-            style = "night"
-        })
-        vim.cmd("colorscheme tokyonight")
-    end
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+            --- You can override specific color groups to use other groups or a hex color
+            --- function will be called with a ColorScheme table
+            ---@param colors ColorScheme
+            on_colors = function(colors) end,
+ 
+            --- You can override specific highlights to use other groups or a hex color
+            --- function will be called with a Highlights and ColorScheme table
+            ---@param highlights Highlights
+            ---@param colors ColorScheme
+            on_highlights = function(highlights, colors) end,
+        },
+        config = function(_, opts) 
+            local tokyonight = require("tokyonight")
+            tokyonight.setup(opts)
+            tokyonight.load()
+        end,
 }
--- nvim/lua/paraskcd/plugins/lsp/mason.lua
+++ nvim/lua/paraskcd/plugins/lsp/mason.lua
@@ -24,16 +24,13 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
         "html",
         "cssls",
         "tailwindcss",
-        "svelte",
         "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        "pyright",
+        "angularls",
+        "csharp_ls",
+        "sonarlint-language-server"
       },
     })
   end,
